a937c16494a5405afe1a46d14a99fb6390d49534
Orban Botond <orbanbotond@gmail.com>
2015-07-07 16:49:22 +0300

    skeleton for login spec is created

---
+++ spec/api/login_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe '/api/login' do
+  context 'negative tests' do
+    context 'missing params' do
+    end
+    context 'invalid params' do
+    end
+  end
+  context 'positive tests' do
+    context 'valid params' do
+    end
+  end
+end
